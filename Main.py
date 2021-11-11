82fd1642c77a75daf32728269f0643e7ff06de99
Alphacodings <94110308+Alphacodings@users.noreply.github.com>
2021-11-11 15:39:31 +0400

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,13 @@
+from turtle import *
+speed (40)
+color ("green")
+ng=Screen()
+ng.bgcolor("black")
+i = 200
+while i>0:
+  left (i)
+  forward(i*3)
+  i = i - 1
+
+#coded by Alpha 
+#tiktok : Alpha.gaming._
